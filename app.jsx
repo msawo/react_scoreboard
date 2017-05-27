a9ef1f5356dc2cb26daf390296d99b684e852c4d
msawo <msawo.inc@gmail.com>
2017-05-27 18:49:26 +0000

    Add transiton to buttons

--- app.jsx
+++ app.jsx
@@ -1,43 +1,68 @@
-function Application(props) {
+function Header(props) {
   return (
-    <div className="scoreboard">
-      <div className="header">
-        <h1>{ props.title }</h1>
+    <div className="header">
+      <h1>{ props.title }</h1>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+}
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
       </div>
-      
-      <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Modou Sawo
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
-        
-        <div className="player">
-          <div className="player-name">
-            Elon Musk
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 33 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
-    
+      <div className="player-score">
+        <Counter score={props.score} />
       </div>
     </div>
   );
 }
 
-ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+}
+
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <Header title={props.title}/>
+      
+      <div className="players">
+        <Player name="Modou Sawo" score={45} />
+        <Player name="Elon Musk" score={47} />
+      </div>
+    </div>
+  );
+}
+
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+  title: "Scoreboard",
+}
+ReactDOM.render(<Application />, document.getElementById('container'));
 
 
 
