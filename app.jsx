2a3ea271a7ea4c63f95ab99e0fe30e659beb46b8
msawo <msawo.inc@gmail.com>
2017-05-28 23:55:35 +0000

    Update th” properiesy of a state object

--- app.jsx
+++ app.jsx
@@ -35,11 +35,13 @@ Header.propTypes = {
 
 
 var Counter = React.createClass({
-  propTypes: {},
+  propTypes: {
+    initialScore: React.PropTypes.number.isRequired,
+  },
   
   getInitialState: function() {
     return {
-      score: 0,
+      score: this.props.initialScore,
     }
   },
   
@@ -75,7 +77,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter />
+        <Counter initialScore={props.score}/>
       </div>
     </div>
   );
