fd9beb8ae86e419cbe5ba7c87577ab1fd90ab82a
msawo <msawo.inc@gmail.com>
2017-06-07 05:14:46 -0400

    Set up app

--- app.jsx
+++ app.jsx
@@ -1,8 +1,6 @@
 function Application() {
   return (
-    <div>
-      <h1>Hello from React</h1>
-      <p>I was rendered from the Application component!</p>
+    <div className="application">
     </div>
   );
 }
